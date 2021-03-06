e78316252ff999531759a62590c88a5aa66c5b3e
wiasliaw <leewei48750@gmail.com>
2021-03-06 18:01:33 +0800

    feat(main.c): feat main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,48 @@
-#include <assert.h>
-#include "adder.h"
+#include <stdlib.h>
+#include "linkedlist.h"
+
+void quicksort(node_t **list)
+{
+    if (!*list)
+        return;
+
+    node_t *pivot = *list;
+    int value = pivot->value;
+    node_t *p = pivot->next;
+    pivot->next = NULL;
+
+    node_t *left = NULL, *right = NULL;
+    while (p) {
+        node_t *n = p;
+        p = p->next;
+        list_add_node_t(n->value > value ? &right : &left, n);
+    }
+
+    quicksort(&left);
+    quicksort(&right);
+
+    node_t *result = NULL;
+    list_concat(&result, left);
+    list_concat(&result, pivot);
+    list_concat(&result, right);
+    *list = result;
+}
 
 int main()
 {
-    assert(5 == add(2, 3));
-    return 0;
+    size_t count = 20;
+
+    node_t *list = NULL;
+    while (count--)
+        list = list_make_node_t(list, random() % 1024);
+
+    list_display(list);
+    quicksort(&list);
+    list_display(list);
+
+    if (!list_is_ordered(list))
+        return EXIT_FAILURE;
+
+    list_free(&list);
+    return EXIT_SUCCESS;
 }
